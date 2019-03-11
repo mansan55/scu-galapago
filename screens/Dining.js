cd90604385096080f0de0695836261eccb3f64ea
Manuel Alejandro Sanchez <ManuelAlejandro@Manuels-MacBook-Air.local>
2019-03-11 01:21:23 -0700

    Adding Dining and Hotels landing pages

---
+++ screens/Dining.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ScrollView, StyleSheet } from 'react-native';
+import { ExpoLinksView } from '@expo/samples';
+import { Container, Header, Content, Card, CardItem, Body, Text } from 'native-base';
+
+
+export default class DiningScreen extends React.Component {
+  static navigationOptions = {
+    title: 'DiningScreen',
+  };
+
+  render() {
+    return (
+      <Container>
+        <Header />
+        <Content>
+          <Card>
+            <CardItem>
+              <Body>
+                <Text>
+                   Text!
+                </Text>
+              </Body>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: '#fff',
+  },
+});
